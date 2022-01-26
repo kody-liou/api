1deb567cca464090677caec00c86d63bf50b2c94
liu-j-h <sqfkw1578@gmail.com>
2022-01-27 06:51:13 +0800

    ts 無法強制 array 必須有什麼東西

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -2,9 +2,41 @@
 require('dotenv').config();
 
 import middy from '@middy/core';
+import Ajv, { JSONSchemaType } from 'ajv';
 import * as middlewares from '../middlewares';
 
+const ajv = new Ajv();
+
+interface MyData {
+  foo: number;
+  bar?: string;
+}
+
+const schema: JSONSchemaType<MyData> = {
+  type: 'object',
+  properties: {
+    foo: { type: 'integer' },
+    bar: { type: 'string', nullable: true },
+  },
+  required: [],
+  additionalProperties: false,
+};
+
+// validate is a type guard for MyData - type is inferred from schema type
+const validate = ajv.compile(schema);
+
+// or, if you did not use type annotation for the schema,
+// type parameter can be used to make it type guard:
+// const validate = ajv.compile<MyData>(schema)
+
 export const profileCreate = middy(async (event: middlewares.Event) => {
+  const a = event.body;
+  if (validate(a)) {
+    // data is MyData here
+    console.log(a);
+  } else {
+    console.log(validate.errors);
+  }
   return {
     statusCode: 200,
     body: '',
