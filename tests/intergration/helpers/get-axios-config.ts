0bde8b923fe354d0b358dd37f3cd784ada16b9b4
liu-j-h <sqfkw1578@gmail.com>
2022-02-14 06:36:48 +0800

    Tidy some code

--- tests/intergration/helpers/get-axios-config.ts
+++ tests/intergration/helpers/get-axios-config.ts
@@ -3,8 +3,13 @@ require('dotenv').config();
 import type { AxiosRequestConfig } from 'axios';
 
 export const getAxiosConfig = (tokenStr: string): AxiosRequestConfig => {
+  const TEST_URL =
+    process.env.RUN_DEV === 'true'
+      ? process.env.LOCAL_API_BASE_URL
+      : process.env.API_BASE_URL;
+  console.log('Test url is:', TEST_URL);
   return {
-    baseURL: process.env.API_BASE_URL,
+    baseURL: TEST_URL,
     timeout: 30000000,
     headers: { Authorization: `Bearer ${tokenStr}` },
   };
--- tests/intergration/helpers/global-setup.ts
+++ tests/intergration/helpers/global-setup.ts
@@ -3,10 +3,7 @@ require('dotenv').config();
 import { setup as setupDevServer } from 'jest-dev-server';
 
 export default async function globalSetup() {
-  if (
-    process.env.API_BASE_URL?.includes('localhost') &&
-    process.env.RUN_DEV === 'true'
-  ) {
+  if (process.env.RUN_DEV === 'true') {
     console.log('global setup');
     await setupDevServer({
       command: 'npm run api:dev',
